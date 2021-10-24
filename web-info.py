51a34188084f32c9487cbd79cd7126540ca4455b
Ranajit199 <92310993+Ranajit199@users.noreply.github.com>
2021-10-24 21:50:46 +0530

    Add files via upload

---
+++ web-info.py
@@ -0,0 +1,51 @@
+print("\033[92m")
+print
+print
+print
+print
+print
+print
+print
+print
+print
+print
+print
+print(""".::!!!!!!!:.
+  .!!!!!:.                        .:!!!!!!!!!!!!
+  ~~~~!!!!!!.                 .:!!!!!!!!!UWWW$$$
+      :$$NWX!!:           .:!!!!!!XUWW$$$$$$$$$P
+      $$$$$##WX!:      .<!!!!UW$$$$"  $$$$$$$$#
+      $$$$$  $$$UX   :!!UW$$$$$$$$$   4$$$$$*
+      ^$$$B  $$$$\     $$$$$$$$$$$$   d$$R"
+        "*$bd$$$$      '*$$$$$$$$$$$o+#""")
+print
+print("__________________________________________________________")
+print
+print("""__    __    ___  ____         ____  ____   _____   ___  
+|  |__|  |  /  _]|    \       |    ||    \ |     | /   \ 
+|  |  |  | /  [_ |  o  ) _____ |  | |  _  ||   __||     |
+|  |  |  ||    _]|     ||     ||  | |  |  ||  |_  |  O  |
+|  `  '  ||   [_ |  O  ||_____||  | |  |  ||   _] |     |
+ \      / |     ||     |       |  | |  |  ||  |   |     |
+  \_/\_/  |_____||_____|      |____||__|__||__|    \___/ 
+                                                         """)
+print
+print("__________________________________________________________")
+print
+print("""DEVELOPED BY : RANAJIT SINGHA (M:8348932574)""")
+print
+print
+print
+import os
+import urllib2
+import sys
+url = raw_input("ENTER WEBSITE LINK : ")
+url.rstrip ( )
+header =urllib2.urlopen (url) .info ( )
+print(str (header) )
+print
+print
+print("""THANK YOU""")
+print
+print
+print
